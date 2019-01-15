3f1ffd210f8b572f3535ea70a7fedc35ee08619d
Mārtiņš Buhanovskis <martins.buhanovskis@gmail.com>
2019-01-15 05:41:37 +0200

    code cleanup

--- tests/googleSearchTest.js
+++ tests/googleSearchTest.js
@@ -29,11 +29,6 @@ module.exports = {
         }); */
         done();
     },
-/* 
-    reporter : function(results, cb) {
-        console.log('results', results);
-        cb();
-    }, */
 
     'Goole Demo Search for nightwatch' : function (browser) {
         browser
@@ -43,47 +38,17 @@ module.exports = {
           .waitForElementVisible('input[name=btnK]', 1000)
           .click('input[name=btnK]')
           .pause(1000)
-/*           .waitForElementVisible('body')
-          .assert.containsText('#main', 'Night Watch')
-          .assert.containsText('.LC20lb','Nightwatch.js | Node.js powered End-to-End testing framework')
-          .click('html body#gsr.srp.tbo.vasq div#main div#cnt.mdm div.mw div#rcnt div.col div#center_col div#res.med div#search div div#ires div#rso div.bkWMgd div.srg div.g div div.rc div.r a')
-          .pause(1000)
-          .waitForElementVisible('body')
-          .assert.containsText('div.col-md-6:nth-child(1) > h1:nth-child(1)','Nightwatch.js')
-          .assert.containsText('div.col-md-6:nth-child(1) > p:nth-child(2)','Browser automated testing, the easy way.')
-          .end(); */
     },
     'Looking at search results and coosing nightwatch homepage' : function (browser) {
         browser
-/*           .url('http://www.google.com')
-          .waitForElementVisible('body')
-          .setValue('input[type=text]', 'nightwatch')
-          .waitForElementVisible('input[name=btnK]')
-          .click('input[name=btnK]')
-          .pause(1000) */
           .waitForElementVisible('body', 1000)
           .assert.containsText('#main', 'Night Watch')
           .assert.containsText('.LC20lb','Nightwatch.js | Node.js powered End-to-End testing framework')
           .click('html body#gsr.srp.tbo.vasq div#main div#cnt.mdm div.mw div#rcnt div.col div#center_col div#res.med div#search div div#ires div#rso div.bkWMgd div.srg div.g div div.rc div.r a')
           .pause(1000)
-/*           .waitForElementVisible('body')
-          .assert.containsText('div.col-md-6:nth-child(1) > h1:nth-child(1)','Nightwatch.js')
-          .assert.containsText('div.col-md-6:nth-child(1) > p:nth-child(2)','Browser automated testing, the easy way.')
-          .end(); */
     },
     'Checking out Nightwatch homepage' : function (browser) {
         browser
-/*           .url('http://www.google.com')
-          .waitForElementVisible('body')
-          .setValue('input[type=text]', 'nightwatch')
-          .waitForElementVisible('input[name=btnK]')
-          .click('input[name=btnK]')
-          .pause(1000)
-          .waitForElementVisible('body')
-          .assert.containsText('#main', 'Night Watch')
-          .assert.containsText('.LC20lb','Nightwatch.js | Node.js powered End-to-End testing framework')
-          .click('html body#gsr.srp.tbo.vasq div#main div#cnt.mdm div.mw div#rcnt div.col div#center_col div#res.med div#search div div#ires div#rso div.bkWMgd div.srg div.g div div.rc div.r a')
-          .pause(1000) */
           .waitForElementVisible('body', 1000)
           .assert.containsText('div.col-md-6:nth-child(1) > h1:nth-child(1)','Nightwatch.js')
           .assert.containsText('div.col-md-6:nth-child(1) > p:nth-child(2)','Browser automated testing, the easy way.')
